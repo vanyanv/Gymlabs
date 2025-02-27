92407025f4a738fb1cbcf5fc7e6768f708458a27
Vardan <vardan.vanyan@gmail.com>
2025-02-27 13:37:57 -0800

    set up workout controller

---
+++ server/controllers/workout.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import prisma from '../lib/prisma';
+
+export const getWorkouts = async (
+  req: Request,
+  res: Response
+): Promise<void> => {};
+
+export const getWorkout = async (
+  req: Request,
+  res: Response
+): Promise<void> => {};
+
+export const createWorkout = async (
+  req: Request,
+  res: Response
+): Promise<void> => {};
+
+export const updateeWorkout = async (
+  req: Request,
+  res: Response
+): Promise<void> => {};
+
+export const deleteWorkout = async (
+  req: Request,
+  res: Response
+): Promise<void> => {};
