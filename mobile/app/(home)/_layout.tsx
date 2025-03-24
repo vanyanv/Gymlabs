476ca4c2f1527ebb0dbce5ea40cdb572b44d84e8
Vardan <vardan.vanyan@gmail.com>
2025-03-24 13:44:49 -0700

    added home page that shows users information and created signout button

--- mobile/app/(home)/_layout.tsx
+++ mobile/app/(home)/_layout.tsx
@@ -1,10 +1,5 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { Stack } from 'expo-router/stack';
 
-export default function _layout() {
-  return (
-    <View>
-      <Text>_layout</Text>
-    </View>
-  );
+export default function Layout() {
+  return <Stack />;
 }
